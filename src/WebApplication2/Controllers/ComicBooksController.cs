e7a4d167fc6bd8d2f760eb43ea716e78071d325c
hanxizhang9910 <linsu1702@gmail.com>
2022-06-20 18:09:50 -0500

    Updated Comic book controller

--- src/WebApplication2/Controllers/ComicBooksController.cs
+++ src/WebApplication2/Controllers/ComicBooksController.cs
@@ -3,31 +3,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication2.Data;
 using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
 {
     public class ComicBooksController : Controller
     {
-        
-        public ActionResult Detail()
-        {
-            var comicBook = new ComicBook() 
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[] 
-                {
-                    new Artist() {Name = "Dan Slott", Role = "Script"},
-                    new Artist() {Name = "Humberto Ramos", Role = "Pencils"},
-                    new Artist() {Name = "Victor Olazaba", Role = "Inks"},
-                    new Artist() {Name = "Edgar Delgado", Role = "Colors"},
-                    new Artist() {Name = "Chris Eliopoulos", Role = "Letters"},
-                }
-            };
+        private ComicBookRepository _comicBookRepository = null;
 
-        return View(comicBook);
-        }   
+        public ComicBooksController()
+        {
+            _comicBookRepository = new ComicBookRepository();
+        }
+        public ActionResult Detail(int? id)
+        {
+            
+            if(id == null)
+            {
+                return HttpNotFound();
+            }
+            var comicBook = _comicBookRepository.GetComicBook(id.Value);
+
+            return View(comicBook);
+        }
+
+        private ActionResult HttpNotFound()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
